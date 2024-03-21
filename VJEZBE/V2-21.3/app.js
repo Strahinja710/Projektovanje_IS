42e3e073db18915024de809f394799d134a48b6a
Strahinja Ostojić <150778851+Strahinja710@users.noreply.github.com>
2024-03-21 09:57:13 +0100

    SO

---
+++ VJEZBE/V2-21.3/app.js
@@ -0,0 +1,19 @@
+function w3_open() {
+  document.getElementById("main").style.marginLeft = "25%";
+  document.getElementById("mySidebar").style.width = "25%";
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("openNav").style.display = "none";
+}
+function w3_close() {
+  document.getElementById("main").style.marginLeft = "0%";
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
+}
+
+async function getVozila() {
+  const response = await fetch("http://localhost:4000/vozila");
+  const data = await response.json;
+  console.log(data);
+}
+
+getVozila();
